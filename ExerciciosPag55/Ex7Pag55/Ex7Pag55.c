be3968e679899281398cae6afae5ec3ad3609cb2
Micaías Viola <micaiasviola@users.noreply.github.com>
2024-08-30 08:44:33 -0300

    v1

--- ExerciciosPag55/Ex7Pag55/Ex7Pag55.c
+++ ExerciciosPag55/Ex7Pag55/Ex7Pag55.c
@@ -4,3 +4,12 @@
 #include<locale.h>
 #include<stdlib.h>
 
+int main() {
+    double r;  // exemplo de raio
+    
+    printf("Insira o raio: ");
+    scanf("%f", &r);
+    double area = M_PI * r * r;
+    printf("A area do círculo com raio %.2f e %.2f\n", r, area);
+    return 0;
+}
